581ea3636f224057a4e7295bf95104957118ca83
Hakan <hbostann@gmail.com>
2020-10-13 14:41:54 +0300

    Create a basic timetable component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
+    <Timings />
   </div>
 </template>
 
 <script>
+import Timings from '@/components/Timings.vue';
 
 export default {
   name: 'App',
   components: {
+    Timings,
   },
 };
 </script>
 
 <style>
 #app {
+  background-color: #f7f4f1;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
