7d264b373c7f08a3707ed2e0d270668660bb79e9
Kadir Cetinkaya <kadircet@google.com>
2020-10-18 18:57:45 +0200

    Implement a dummy TimingsServer

---
+++ backend/server/server.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"context"
+	"log"
+	protos "pitwall/backend/protos"
+	"time"
+)
+
+type TimingsServer struct {
+	protos.UnimplementedTimingsServer
+}
+
+func (s *TimingsServer) Session(ctx context.Context, req *protos.EmptyRequest) (*protos.SessionReply, error) {
+	reply := protos.SessionReply{
+		Session: &protos.Session{
+			Participants: []*protos.Participant{
+				{
+					Name: "test_driver1",
+				},
+				{
+					Name: "test_driver2",
+				},
+				{
+					Name: "test_driver3",
+				},
+			},
+		},
+	}
+	return &reply, nil
+}
+
+func (s *TimingsServer) RaceUpdates(req *protos.EmptyRequest, stream protos.Timings_RaceUpdatesServer) error {
+	reply := protos.CarReply{
+		Cars: []*protos.Car{
+			{
+				Position: &protos.Car_WorldPosition{
+					X: 0,
+					Y: 0,
+				},
+			},
+			{
+				Position: &protos.Car_WorldPosition{
+					X: 1,
+					Y: 0,
+				},
+			},
+			{
+				Position: &protos.Car_WorldPosition{
+					X: 2,
+					Y: 0,
+				},
+			},
+		},
+	}
+	for {
+		if err := stream.Send(&reply); err != nil {
+			log.Print(err)
+			return err
+		}
+		for _, car := range reply.Cars {
+			car.Position.X += 1
+			car.Position.Y += 1
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
